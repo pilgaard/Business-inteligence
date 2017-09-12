03f60c73087180f362ca01ef88cf66456b85ace4
pilgaard <emil.pilgaard@gmail.com>
2017-09-12 21:14:50 +0200

    .

--- assignment_2.py
+++ assignment_2.py
@@ -3,6 +3,25 @@ import requests
 import csv
 import os
 
+url = "http://138.197.184.35/boliga/"
+def scrape_index(url):
+
+    indexes = []
+
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.content.decode('utf-8'), 'html5lib')
+
+    table = soup.find('table')
+    table_body = table.find('tbody')
+
+    links = table_body.find_all('a')
+    del links [:5]
+
+#    for link in links:
+#        print(link.get("href"))
+
+    return links
+
 def save_to_csv(data, path='./out/boliga.csv'):
 
     with open(path, 'w', encoding='utf-8') as output_file:
