ffcc55e202cbe67c92716c0f9ce3c4af2bc8e1e6
pilgaard <emil.pilgaard@gmail.com>
2017-09-24 16:56:05 +0200

    .

--- assignment_3/ass.py
+++ assignment_3/ass.py
@@ -24,16 +24,19 @@ def save_geolocations(decoded_nodes):
     lon = []
 
     for idx, decoded_node in enumerate(decoded_nodes):
+        #if idx>10:
+        #    break
         houses = houses.append(pd.DataFrame(decoded_node.tags, columns=['addr:street', 'addr:postcode', 'addr:city', 'addr:housenumber'],index=[idx]))
         lat.append(decoded_node.lat)
         lon.append(decoded_node.lon)
     df = pd.DataFrame({'lat': lat, 'lon': lon}, columns=['lat', 'lon'])
-    #houses.join(df)
+    houses.columns = ['street', 'zipcode', 'city', 'addr:housenumber']
+    houses["street"] = houses["street"].map(str) +" "+ houses["addr:housenumber"]
     return pd.merge(houses, df, left_index=True, right_index=True)
 
 
 def run():
     results = save_geolocations(decode_node_to_csv())
-    results.to_csv("decoded_nodes.csv", encoding='utf-8')
+    results.to_csv("decoded_nodes.csv")
 
 run()
